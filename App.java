8f4c11d731c1bb0924c561c41e5593f9a03c8148
Demetrius <2004.viper@gmail.com>
2016-12-25 20:39:10 +0300

    create Application

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+
+        public static void main(String[] args){
+              if (null != args[0]) {
+              System.out.println("Hello "+ args[0]);
+              } else
+                System.out.println("PSH-PSH ALLO!");
+        }
+
+
+
+}
+
+
+
