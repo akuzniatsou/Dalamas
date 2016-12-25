9db2911728244e99593a969cd2b2874d0b7f89a9
Demetrius <2004.viper@gmail.com>
2016-12-25 21:39:00 +0300

    add gradle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,6 @@
 public class App {
 
         public static void main(String[] args){
-              if (null != args[0]) {
-              System.out.println("Hello "+ args[0]);
-              } else
                 System.out.println("PSH-PSH ALLO!");
         }
 
